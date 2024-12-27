53094582bc35aa953d34c6dfe684231bd5ce4e3e
simorai <109289730+simorai@users.noreply.github.com>
2024-12-27 18:05:00 +0000

    Add project files.

---
+++ StringBuilder/Entities/Comments.cs
@@ -0,0 +1,16 @@
+﻿namespace StringBuilder.Entities
+{
+    class Comments
+    {
+        public string Text { get; set; }
+
+        public Comments() { }
+
+        public Comments(string text)
+        {
+            Text = text;
+        }
+
+
+    }
+}
---
+++ StringBuilder/Entities/Post.cs
@@ -0,0 +1,10 @@
+﻿namespace StringBuilder.Entities
+{
+    class Post
+    {
+        public DateTime Moment { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int Likes { get; set; }
+    }
+}
